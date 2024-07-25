a4c6e89d0c70b54ea79175aa367358ea6d64d354
ZehelFenris <43239740+ZehelFenris@users.noreply.github.com>
2024-07-25 17:17:30 +0200

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,74 @@
+---@class BetterBags: AceAddon
+local addon = LibStub('AceAddon-3.0'):GetAddon("BetterBags")
+
+---@class Categories: AceModule
+local categories = addon:GetModule('Categories')
+
+---@class Localization: AceModule
+local L = addon:GetModule('Localization')
+
+
+categories:AddItemToCategory(206374, L:G("Naxx crafting")) --Invader's Scourgestone
+categories:AddItemToCategory(206375, L:G("Naxx crafting")) --Corruptor's Scourgestone
+categories:AddItemToCategory(206576, L:G("Naxx crafting")) --Death's Bargaining Chip
+categories:AddItemToCategory(206579, L:G("Naxx crafting")) --Phylacterweave
+categories:AddItemToCategory(206645, L:G("Naxx crafting")) --Cursed Cloth
+categories:AddItemToCategory(206771, L:G("Naxx crafting")) --Pattern: Cursed Cloth
+categories:AddItemToCategory(206646, L:G("Naxx crafting")) --Languished Leather
+categories:AddItemToCategory(206772, L:G("Naxx crafting")) --Pattern: Languished Leather
+categories:AddItemToCategory(206647, L:G("Naxx crafting")) --Scourged Scales
+categories:AddItemToCategory(206773, L:G("Naxx crafting")) --Pattern: Scourged Scales
+categories:AddItemToCategory(206648, L:G("Naxx crafting")) --Undeath Metal
+categories:AddItemToCategory(206774, L:G("Naxx crafting")) --Pattern: Undeath Metal
+categories:AddItemToCategory(206577, L:G("Naxx crafting")) --Slime-Covered Scroll
+
+--Lamented Crusader's
+categories:AddItemToCategory(206604, L:G("Naxx lamented crusader's")) --Lamented Crusader's Healmet
+categories:AddItemToCategory(206605, L:G("Naxx lamented crusader's")) --Lamented Crusader's Spaulders
+categories:AddItemToCategory(206606, L:G("Naxx lamented crusader's")) --Lamented Crusader's Chestpiece
+categories:AddItemToCategory(206607, L:G("Naxx lamented crusader's")) --Lamented Crusader's Bracers
+categories:AddItemToCategory(206608, L:G("Naxx lamented crusader's")) --Lamented Crusader's Gauntlets
+categories:AddItemToCategory(206609, L:G("Naxx lamented crusader's")) --Lamented Crusader's Belt
+categories:AddItemToCategory(206610, L:G("Naxx lamented crusader's")) --Lamented Crusader's Leggings
+categories:AddItemToCategory(206611, L:G("Naxx lamented crusader's")) --Lamented Crusader's Boots
+
+--Desecrated cloth
+categories:AddItemToCategory(206612, L:G("Naxx desecrated cloth")) --Desecrated Cloth Healmet
+categories:AddItemToCategory(206613, L:G("Naxx desecrated cloth")) --Desecrated Cloth Spaulders
+categories:AddItemToCategory(206614, L:G("Naxx desecrated cloth")) --Desecrated Cloth Chestpiece
+categories:AddItemToCategory(206615, L:G("Naxx desecrated cloth")) --Desecrated Cloth Bracers
+categories:AddItemToCategory(206616, L:G("Naxx desecrated cloth")) --Desecrated Cloth Gauntlets
+categories:AddItemToCategory(206617, L:G("Naxx desecrated cloth")) --Desecrated Cloth Belt
+categories:AddItemToCategory(206618, L:G("Naxx desecrated cloth")) --Desecrated Cloth Leggings
+categories:AddItemToCategory(206619, L:G("Naxx desecrated cloth")) --Desecrated Cloth Boots
+
+--Desecrated Leather
+categories:AddItemToCategory(206620, L:G("Naxx desecrated leather")) --Desecrated Leather Healmet
+categories:AddItemToCategory(206621, L:G("Naxx desecrated leather")) --Desecrated Leather Spaulders
+categories:AddItemToCategory(206622, L:G("Naxx desecrated leather")) --Desecrated Leather Chestpiece
+categories:AddItemToCategory(206623, L:G("Naxx desecrated leather")) --Desecrated Leather Bracers
+categories:AddItemToCategory(206624, L:G("Naxx desecrated leather")) --Desecrated Leather Gauntlets
+categories:AddItemToCategory(206625, L:G("Naxx desecrated leather")) --Desecrated Leather Belt
+categories:AddItemToCategory(206626, L:G("Naxx desecrated leather")) --Desecrated Leather Leggings
+categories:AddItemToCategory(206627, L:G("Naxx desecrated leather")) --Desecrated Leather Boots
+
+--Desecrated Mail
+categories:AddItemToCategory(206628, L:G("Naxx desecrated mail")) --Desecrated Mail Healmet
+categories:AddItemToCategory(206629, L:G("Naxx desecrated mail")) --Desecrated Mail Spaulders
+categories:AddItemToCategory(206630, L:G("Naxx desecrated mail")) --Desecrated Mail Chestpiece
+categories:AddItemToCategory(206631, L:G("Naxx desecrated mail")) --Desecrated Mail Bracers
+categories:AddItemToCategory(206632, L:G("Naxx desecrated mail")) --Desecrated Mail Gauntlets
+categories:AddItemToCategory(206633, L:G("Naxx desecrated mail")) --Desecrated Mail Belt
+categories:AddItemToCategory(206634, L:G("Naxx desecrated mail")) --Desecrated Mail Leggings
+categories:AddItemToCategory(206635, L:G("Naxx desecrated mail")) --Desecrated Mail Boots
+
+--Desecrated Plate
+categories:AddItemToCategory(206636, L:G("Naxx desecrated plate")) --Desecrated Plate Healmet
+categories:AddItemToCategory(206637, L:G("Naxx desecrated plate")) --Desecrated Plate Spaulders
+categories:AddItemToCategory(206639, L:G("Naxx desecrated plate")) --Desecrated Plate Chestpiece
+categories:AddItemToCategory(206640, L:G("Naxx desecrated plate")) --Desecrated Plate Bracers
+categories:AddItemToCategory(206641, L:G("Naxx desecrated plate")) --Desecrated Plate Gauntlets
+categories:AddItemToCategory(206642, L:G("Naxx desecrated plate")) --Desecrated Plate Belt
+categories:AddItemToCategory(206643, L:G("Naxx desecrated plate")) --Desecrated Plate Leggings
+categories:AddItemToCategory(206644, L:G("Naxx desecrated plate")) --Desecrated Plate Boots
+
